639afe46aab6e8a1b8399e6fba49bb0d847b42f8
Chatpong Deepet <chatpond@outlook.com>
2025-01-08 20:02:28 +0700

    omplete redux

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import "./App.css";
 import { useDispatch } from "react-redux";
 import { USER_TYPE } from "./store/UserReducer";
 import UserDisplay from "./UserDisplay";
+import { log } from "console";
+import { POST_TYPE } from "./store/PostReducer";
+import PostDisplay from "./PostDisplay";
 
 function App() {
   const [userid, setUserid] = useState(0);
   const dispatch = useDispatch(); // ใช้ Redux Hook เพื่อรับฟังก์ชัน dispatch
+  const [postid, setPostid] = useState(0);
 
   const onChangeUserId = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const useridFromInput = e.target.value ? Number(e.target.value) : 0;
@@ -42,13 +46,53 @@ function App() {
     }
   };
 
+  const onChangePostId = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const postIdFromInput = e.target.value ? Number(e.target.value) : 0;
+    setPostid(postIdFromInput);
+
+    const postResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/" + postIdFromInput
+    );
+
+    if (postResponse.ok) {
+      const post = await postResponse.json();
+      console.log(post);
+      dispatch({
+        type: POST_TYPE,
+        payload: {
+          id: post.id,
+          title: post.titile,
+          body: post.body,
+        },
+      });
+    }
+  };
+
   return (
     <React.Fragment>
-      <div className="App">
-        <label>user id</label>
-        <input value={userid} onChange={onChangeUserId} placeholder="user id" />
+      <div style={{ width: "300px" }}>
+        <div className="App">
+          <label>user id</label>
+          <input
+            value={userid}
+            onChange={onChangeUserId}
+            placeholder="user id"
+          />
+        </div>
+        <UserDisplay />
       </div>
-      <UserDisplay />
+      <br />
+      <div style={{ width: "300px" }}>
+        <div className="App">
+          <label>post id</label>
+          <input
+            value={postid}
+            onChange={onChangePostId}
+            placeholder="input id"
+          />
+        </div>
+        <PostDisplay />
+      </div>
     </React.Fragment>
   );
 }
